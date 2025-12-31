103c3d4bc489e62d01d8d91c5978ebd42f613623
Guilherme <guilhermesaar02@gmail.com>
2025-12-31 14:33:10 -0300

    minor AssistantConfig change

--- src/main/java/com/gstech/pdfAssistant/config/AssistantConfig.java
+++ src/main/java/com/gstech/pdfAssistant/config/AssistantConfig.java
@@ -1,13 +1,33 @@
 package com.gstech.pdfAssistant.config;
 
 import com.gstech.pdfAssistant.interfaces.AssistantFreeTalk;
+import com.gstech.pdfAssistant.interfaces.AssistantPDF;
 import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
 import dev.langchain4j.service.AiServices;
+import dev.langchain4j.store.embedding.mongodb.MongoDbEmbeddingStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AssistantConfig {
+
+    @Bean
+    public AssistantPDF assistant(ChatModel chatModel, EmbeddingModel embeddingModel, MongoDbEmbeddingStore embeddingStore) {
+
+        return AiServices.builder(AssistantPDF.class)
+                .chatModel(chatModel)
+                .contentRetriever(
+                        EmbeddingStoreContentRetriever.builder()
+                                .embeddingModel(embeddingModel)
+                                .embeddingStore(embeddingStore)
+                                .maxResults(12)
+                                .minScore(0.60)
+                                .build()
+                )
+                .build();
+    }
 
     @Bean
     public AssistantFreeTalk assistantFreeTalk(ChatModel chatModel) {
